d4acf7cf6acddec5cdb04a5cb1469e77324da035
kristaps <you@example.com>
2024-11-04 10:35:42 +0200

    fix broken code part

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,24 +1,29 @@
 @extends('layouts.app', ['title' => 'Izveidot produktu'])
 
 @section('content')
-    <h1>Izveidot produktu</h1>
+<h1>Izveidot produktu</h1>
 
-    <form method="post" action="{{ route('products.store') }}">
-        @csrf
-        <div class="mb-3">
-            <label for="nameInput" class="form-label">Nosaukums</label>
-            <input type="text" class="form-control" id="nameInput" name="name"value="{{ old('name') }}">
-        </div>
-        <div class="mb-3">
-            <label for="qtyInput" class="form-label">Daudzums</label>
-            <input type="text" class="form-control" id="qtyInput" name="qty" value="{{ old('qty') }}">
-        </div>
-        <div class="mb-3">
-            <label for="descriptionInput" class="form-label">Apraksts</label>
-            <input type="text" class="form-control" id="descriptionInput" name="description" value="{{ old('description') }}">
-        </div>
-        <div class="mb-3">
-            <input type="submit" class="btn btn-primary" value="Saglabāt">
-        </div>
-    </form>
+<form method="post" action="{{ route('products.store') }}">
+    @csrf
+    <div class="mb-3">
+        <label for="nameInput" class="form-label">Nosaukums</label>
+        <input type="text" class="form-control" id="nameInput" name="name" value="{{ old('name') }}">
+    </div>
+    <div class="mb-3">
+        <label for="qtyInput" class="form-label">Daudzums</label>
+        <input type="text" class="form-control" id="qtyInput" name="qty" value="{{ old('qty') }}">
+    </div>
+    <div class="mb-3">
+        <label for="priceInput" class="form-label">Cena</label>
+        <input type="text" class="form-control" id="priceInput" name="price" value="{{ old('price') }}">
+    </div>
+    <div class="mb-3">
+        <label for="descriptionInput" class="form-label">Apraksts</label>
+        <input type="text" class="form-control" id="descriptionInput" name="description"
+            value="{{ old('description') }}">
+    </div>
+    <div class="mb-3">
+        <input type="submit" class="btn btn-primary" value="Saglabāt">
+    </div>
+</form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
+Route::get('products', [ProductController::class, 'index'])->name('products.index');
 Route::post('products', [ProductController::class, 'store'])->name('products.store');
 Route::get('products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
 Route::put('products/{product}/update', [ProductController::class, 'update'])->name('products.update');
